8cd682e7b5711b8f1cb0730f3411c406d6dac6c7
apcortez <a.cortez@campus.unimib.it>
2021-07-12 12:42:53 +0200

    Aggiungere i file di progetto.

---
+++ PrimoProgetto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimoProgetto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ciao a tutte");
+        }
+    }
+}
